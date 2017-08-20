b654fbad4fcdc266a7ebff14118e4f151908e0b5
Mikey <byqj3b@126.com>
2017-08-20 19:10:53 +0800

    创建项目

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by Administrator on 2017/8/19 0019.
  */
+;(function (angular) {
+    var app = angular.module("dfs",[]);
+    app.controller("dfsController",["$scope",function ($scope) {
+        $scope.msg="abc"
+    }])
+})(angular);
